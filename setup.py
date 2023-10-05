8910503eca1f1ebde07ec34dd98fe6e06bc5dad7
Lirui Wang (Leroy) <liruiw@mit.edu>
2023-10-05 16:12:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cliport',
+    name='gensim',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license=open('LICENSE').read(),
     zip_safe=False,
-    description="CLIPort - What and Where Pathways for Robotic Manipulation.",
-    author='Mohit Shridhar',
-    author_email='mshr@cs.washington.edu',
-    url='https://cliport.github.io/',
+    description="GenSim: Generating Robotic Simulation Tasks via Large Language Models.",
+    author='Lirui Wang',
+    author_email='liruiw@mit.edu',
+    url='https://liruiw.github.io/gensim',
    #  install_requires=[line for line in open('requirements.txt').readlines() if "@" not in line],
-    keywords=['CLIP', 'Vision Language Grounding', 'Robotics', 'Manipulation'],
+    keywords=['Large Language Models', 'Simulation', 'Vision Language Grounding', 'Robotics', 'Manipulation'],
 )
